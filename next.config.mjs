9d4a15891c11f449022836886eca23315378a562
mikohan <angara99@gmail.com>
2026-04-30 19:19:51 -0700

    Added nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Enables automatic CSS inlining to fix render-blocking warnings
+  experimental: {
+    optimizeCss: true,
+  },
+  // Since you are running on port 3003, ensure your environment is set for production
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
 export default nextConfig
